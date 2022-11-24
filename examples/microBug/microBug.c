025ea022826f1c35d190c67f33fd37be11d7d729
SonicStark <50692172+SonicStark@users.noreply.github.com>
2022-11-24 02:54:05 +0000

    Several changes of `microBug`
     - Compile x86 targets
     - Improve `microBug.c`

--- examples/microBug/microBug.c
+++ examples/microBug/microBug.c
@@ -1,13 +1,16 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
-void use_sprintf(char* pBuf)
+void call_sprintf(char* pBuf)
 {
+    printf(" => call_sprintf"); fflush(stdout);
     sprintf(pBuf, "abcdefghijklmnopqrstuvwxyz");
 }
 
-void use_strftime(char* pBuf)
+void call_strftime(char* pBuf)
 {
+    printf(" => call_strftime"); fflush(stdout);
     time_t rawtime;
     time(&rawtime);
     struct tm *info;
@@ -15,22 +18,48 @@ void use_strftime(char* pBuf)
     strftime(pBuf, 64, "%Y%m%d%H%M%S0123456789abcdefg", info);
 }
 
-void use_assignment(char* pBuf)
+void call_write(char* pBuf, int wlen)
 {
-    for (int __i=0; __i<=256; __i++)
+    printf(" => call_write_L%d", wlen); fflush(stdout);
+    for (int __i=0; __i<=wlen; __i++)
         { pBuf[__i] = 0x88; }
 }
 
-void use_all(char* pBuf)
+void hit_HeapBufferOverflow(char* pBuf, int wlen)
 {
-    use_sprintf(pBuf);
-    use_strftime(pBuf);
-    use_assignment(pBuf);
+    for (int __i=0; __i<=wlen; __i++)
+    {
+        call_sprintf(pBuf);
+        call_strftime(pBuf);
+        call_write(pBuf, __i);
+    }
 }
 
-int main()
+void hit_HeapUseAfterFree(char* pBuf, int wlen)
 {
-    char overflow_buf[64];
-    use_all(overflow_buf);
+    free(pBuf);
+    for (int __i=0; __i<=wlen; __i++)
+    {
+        call_sprintf(pBuf);
+        call_strftime(pBuf);
+        call_write(pBuf, __i);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    printf(" => main");
+
+    int wLength;
+    if (argc != 2) { puts(" => argc exit"); return 0; }
+    else { wLength = atoi(argv[1]); }
+
+    char* pBadBuffer = (char*) malloc(128*sizeof(char));
+
+    if      (wLength >= 66 && wLength <= 88 ) { hit_HeapUseAfterFree   (pBadBuffer, wLength); }
+    else if (wLength >= 99 && wLength <= 666) { hit_HeapBufferOverflow (pBadBuffer, wLength); }
+    else { call_sprintf(pBadBuffer); free(pBadBuffer); }
+    
+    puts(" => normal exit");
     return 0;
 }

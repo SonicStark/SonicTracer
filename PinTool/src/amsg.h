8d7a115f08f887b39f43d321a1ac7b763300972f
SonicStark <50692172+SonicStark@users.noreply.github.com>
2022-11-24 15:47:16 +0000

    Add message code definitions

---
+++ PinTool/src/amsg.h
@@ -0,0 +1,11 @@
+#ifndef HEAD_EMSG_H
+#define HEAD_EMSG_H
+
+#define EVIL_ARG (INT32) -1
+#define GOOD_ARG (INT32) 0
+
+#define TL_INS (INT32) 100
+#define TL_BBL (INT32) 200
+#define TL_CAL (INT32) 300
+
+#endif

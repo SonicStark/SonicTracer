2512fe04d93fd608416abeb32889edbf36aec9c9
SonicStark <50692172+SonicStark@users.noreply.github.com>
2022-11-30 05:44:11 +0000

    Complete `instrumentation` and `analysis` routines
    Prototype was added at commit a4e38450d6c764b47ca102521993bfe966f82c30

--- PinTool/src/payload.cpp
+++ PinTool/src/payload.cpp
@@ -1,4 +1,6 @@
 #include "payload.h"
+#include "checker.h"
+#include "cli.h"
 
 /**
  * Please refer to:
@@ -17,50 +19,109 @@
  * utilizes the RTN_AddInstrumentFunction API call.
  */
 
-///////// INSTRUCTION   LEVEL /////////
-/**
- * 
- */
-AFUNPTR InstrumINS_Dat(){}
+SymPtrVector SymPtrLst; //manage string pointers
+PIN_LOCK WriteFile; //the lock used in writing files
 
 /**
- * 
+ * Analyse Routine for saving address
+ * @param addr memory address
  */
-AFUNPTR InstrumINS_DatSym(){}
+AFUNPTR SaveDat(ADDRINT addr)
+{
+    PIN_GetLock(&WriteFile, WriteFile._owner);
+    TrDat << hexstr(addr) << std::endl;
+    PIN_ReleaseLock(&WriteFile);
+}
 
 /**
- * 
+ * Analyse Routine for saving address, thread-ID and symbol string
+ * @param addr memory address
+ * @param tidv thread ID
+ * @param psym pointer of a symbol string
  */
-INS_INSTRUMENT_CALLBACK AnalyseINS(INS Iparam, VOID *Vparam){}
-
-///////// BASIC BLOCK   LEVEL /////////
-/**
- * 
- */
-AFUNPTR InstrumBBL_Dat(){}
-
-/**
- * 
- */
-AFUNPTR InstrumBBL_DatSym(){}
+AFUNPTR SaveDatSym(ADDRINT addr, PIN_THREAD_UID tidv, std::string *psym)
+{
+    PIN_GetLock(&WriteFile, WriteFile._owner);
+    TrDat << hexstr(addr) << std::endl;
+    TrSym << hexstr(tidv) << "," << *psym << std::endl;
+    PIN_ReleaseLock(&WriteFile);
+}
 
 /**
- * 
+ * Instrumentation Routine at instruction-level
+ * @param Iparam Instruction Object
+ * @param Vparam from default signature & unused
  */
-TRACE_INSTRUMENT_CALLBACK AnalyseBBL(TRACE Tparam, VOID *Vparam){}
+INS_INSTRUMENT_CALLBACK AnalyseINS(INS Iparam, VOID *Vparam)
+{
+    ADDRINT ins_addr = INS_Address(Iparam);
+    if (!IsInsideMain(ins_addr)) { return; }
+    else {
+        if (IsBlocked(ins_addr)) { return; }
+        else {
+            if (!TrSym.is_open()) {
+                INS_InsertCall(Iparam, IPOINT_BEFORE, SaveDat, 
+                        ins_addr, 
+                    IARG_END);
+            } else {
+                std::string ins_name = RTN_FindNameByAddress(ins_addr);
+                INS_InsertCall(Iparam, IPOINT_BEFORE, SaveDatSym,
+                        ins_addr, PIN_ThreadUid(), SymPtrLst.GetSymPtr(ins_name), 
+                    IARG_END);
+            }
+        }
+    }
+}
 
-///////// FUNCTION CALL LEVEL /////////
 /**
- * 
+ * Instrumentation Routine at basic-block-level
+ * @param Tparam TRACE Object
+ * @param Vparam from default signature & unused
  */
-AFUNPTR InstrumCAL_Dat(){}
+TRACE_INSTRUMENT_CALLBACK AnalyseBBL(TRACE Tparam, VOID *Vparam)
+{
+    for (BBL B__=TRACE_BblHead(Tparam); BBL_Valid(B__); B__=BBL_Next(B__)){
+        ADDRINT bbl_addr = BBL_Address(B__);
+        if (!IsInsideMain(bbl_addr)) { continue; }
+        else {
+            if (IsBlocked(bbl_addr)) { continue; }
+            else {
+                if (!TrSym.is_open()) {
+                    BBL_InsertCall(B__, IPOINT_BEFORE, SaveDat,
+                        bbl_addr,
+                    IARG_END);
+                } else {
+                    std::string bbl_name = RTN_FindNameByAddress(bbl_addr);
+                    BBL_InsertCall(B__, IPOINT_BEFORE, SaveDat,
+                        bbl_addr, PIN_ThreadUid(), SymPtrLst.GetSymPtr(bbl_name),
+                    IARG_END);
+                }
+            }
+        }
+    }
+}
 
 /**
- * 
+ * Instrumentation Routine at function-call-level
+ * @param Rparam Routine Object
+ * @param Vparam from default signature & unused
  */
-AFUNPTR InstrumCAL_DatSym(){}
-
-/**
- * 
- */
-RTN_INSTRUMENT_CALLBACK AnalyseCAL(RTN Rparam, VOID *Vparam){}
+RTN_INSTRUMENT_CALLBACK AnalyseCAL(RTN Rparam, VOID *Vparam)
+{
+    if (!IsInsideMain(Rparam)) { return; }
+    else {
+        if (IsBlocked(Rparam)) { return; }
+        else {
+            if (!TrSym.is_open()) {
+                RTN_InsertCall(Rparam, IPOINT_BEFORE, SaveDat,
+                        RTN_Address(Rparam), 
+                    IARG_END);
+            } else {
+                std::string rname = RTN_Name(Rparam);
+                RTN_InsertCall(Rparam, IPOINT_BEFORE, SaveDatSym,
+                        RTN_Address(Rparam), PIN_ThreadUid(), SymPtrLst.GetSymPtr(rname),
+                    IARG_END);
+            }
+        }
+    }
+}
